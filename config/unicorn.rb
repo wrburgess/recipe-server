49fb7c90c9e8a7a33c3363b04f387d38b114d246
Randy Burgess <wrburgess@wrburgess.com>
2013-05-08 14:29:28 -0500

    adding Procfile for Foreman and setting up unicorn config for issue #20

---
+++ config/unicorn.rb
@@ -0,0 +1,14 @@
+worker_processes 4
+timeout 30
+preload_app true
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+  Rails.logger.info('Disconnected from ActiveRecord')
+  sleep 1
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+  Rails.logger.info('Connected to ActiveRecord')
+end
