af89413315d07df5c3da59dcf1bf73f4dbb41c78
Randy Burgess <wrburgess@wrburgess.com>
2013-05-09 15:45:33 -0500

    adding seeds for tools

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@ CSV.foreach("#{Rails.root}/db/fill/ingredients.csv", :headers => :first_row) do 
   puts "Ingredient created: #{row[0]}, #{row[1]}}"
 end
 
+CSV.foreach("#{Rails.root}/db/fill/tools.csv", :headers => :first_row) do |row|
+  Tool.create!({ name: row[0], description: row[1]}, without_protection: true)
+  puts "Tool created: #{row[0]}, #{row[1]}}"
+end
+
 CSV.foreach("#{Rails.root}/db/fill/recipes.csv", :headers => :first_row) do |row|
   Recipe.create!(name: row[0], description: row[1])
   puts "Recipe created: #{row[0]}, #{row[1]}"
@@ -18,5 +23,10 @@ end
 CSV.foreach("#{Rails.root}/db/fill/step_ingredients.csv", :headers => :first_row) do |row|
   StepIngredient.create!({ step_id: row[0], ingredient_id: row[1], volume: row[2]}, without_protection: true)
   puts "Step Ingredient created: #{row[0]}, #{row[1]}, #{row[2]}}"
+end
+
+CSV.foreach("#{Rails.root}/db/fill/step_tools.csv", :headers => :first_row) do |row|
+  StepTool.create!({ step_id: row[0], tool_id: row[1]}, without_protection: true)
+  puts "Step Tool created: #{row[0]}, #{row[1]}, #{row[2]}}"
 end
 
