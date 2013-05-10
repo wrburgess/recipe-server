c74d1c739846b74268eb64083fa59e3824091479
Randy Burgess <wrburgess@wrburgess.com>
2013-05-10 09:37:02 -0500

    updating new_relic to 3.6.2.96

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ RecipeServer::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  namespace :admin do
-
-  end
-
   namespace :api do
     namespace :v1 do
       resources :recipes
