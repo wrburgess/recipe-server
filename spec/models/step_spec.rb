0922b6c410d5937e6f830d13c3685a3423abe3c3
Randy Burgess <wrburgess@wrburgess.com>
2013-05-12 16:04:42 -0500

    updating step_spec to use new expect api

--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Step do
 
   it 'has a valid factory' do
-    FactoryGirl.create(:step).should be_valid
+    step = FactoryGirl.create(:step)
+    expect(step).to be_valid
   end
 
   it { should belong_to(:recipe) }
