23bf1539717e8eb299c5b91200a6deb3d11c2304
Randy Burgess <wrburgess@wrburgess.com>
2013-05-12 16:15:18 -0500

    updating step_tool spec to use expect

--- spec/models/step_tool_spec.rb
+++ spec/models/step_tool_spec.rb
@@ -3,15 +3,18 @@ require 'spec_helper'
 describe StepTool do
 
   it 'has a valid factory' do
-    FactoryGirl.create(:step_tool).should be_valid
+    step_tool = FactoryGirl.create(:step_tool)
+    expect(step_tool).to be_valid
   end
 
   it 'is invalid without a step_id' do
-     FactoryGirl.build(:step_tool, step_id: nil).should_not be_valid
+     step_tool = FactoryGirl.build(:step_tool, step_id: nil)
+     expect(step_tool).to be_invalid
    end
 
   it 'is invalid without an tool_id' do
-    FactoryGirl.build(:step_tool, tool_id: nil).should_not be_valid
+    step_tool = FactoryGirl.build(:step_tool, tool_id: nil)
+    expect(step_tool).to be_invalid
   end
 
   it { should belong_to(:step) }
