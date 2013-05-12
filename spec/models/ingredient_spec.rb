6a762c750ea6618646328410d9b03e205cbaac29
Randy Burgess <wrburgess@wrburgess.com>
2013-05-12 16:19:09 -0500

    updating ingredient spec to use expect

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Ingredient do
 
   it 'has a valid factory' do
-    FactoryGirl.create(:ingredient).should be_valid
+    ingredient = FactoryGirl.create(:ingredient)
+    expect(ingredient).to be_valid
   end
 
   it { should have_many(:step_ingredients) }
