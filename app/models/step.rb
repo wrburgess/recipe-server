b9101f4e768cbe5b8e624f123dc1fc23c7a6b31f
Randy Burgess <wrburgess@wrburgess.com>
2013-05-18 13:21:52 -0500

    tweaks for mock and step assocs

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,8 +2,10 @@ class Step < ActiveRecord::Base
   attr_accessible :name, :description, :weight
 
   belongs_to :recipe
+  
   has_many :step_tools
   has_many :tools, through: :step_tools
-  has_many :step_tools
+
+  has_many :step_ingredients
   has_many :ingredients, through: :step_ingredients
 end
